339a742eda266874182b34aefaaa991ed55ef72d
Kelly Ceriani <Kelly Ceriani>
2016-08-01 17:47:48 -0400

    css/

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
 	$('.additem').on("click", function(event) {
-		alert("Hi");
 		event.preventDefault();
-		console.log("Hi" + event);
+		var something = $('.inputbox').val();
+		//if user inputs nothing and hits enter alert will pop up
+		if (something === "") {
+			alert('Please enter an item!')
+		}
+		//otherwise text appended as a li to shopping list
+		else {
+			$('.listcontainer ul').append("<li class='current-item'><img class='checked' src='images/check-mark.png'/>" + "" + something + "" + "<img class='crossed' src='images/cross.png'/></li>");
+		};
+		//enter key hit 
+		$(document).keydown(function(e) {
+			if (e.keyCode == 13) {
+				$('.listcontainer ul').append("<li class='current-item'><img class='checked' src='images/check-mark.png'/>" + "" + something + "" + "<img class='crossed' src='images/cross.png'/></li>");
+			};			
+		});
+	//resets the textbox after item entered
+	$('#useradd')[0].reset();
 	});
-		
 });
+
+//functions to delete and striketrough code
+assignlisteners();
+function assignlisteners () {
+	 $('ul').on('click', '.crossed', function(event) {
+	 	console.log('trash clicked');
+		$(this).closest('li').remove();
+	});
+	$('ul').on('click', '.checked', function(event) {
+		$(this).closest('li').toggleClass('linethrough');
+	});
+}
